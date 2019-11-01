f97b87c35010cd26bc3c2ca27f965e129612949b
PoloDev41 <paulalexandre.peyrat@gmail.com>
2019-11-01 15:35:56 +0100

    Add unit tests project

---
+++ WebCiv/EngineUnitTest/UnitTest1.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.TestHost;
+using Moq;
+using NUnit.Framework;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebCiv;
+
+namespace EngineUnitTest
+{
+    public class Tests
+    {
+        private TestServer _server;
+        private HttpClient _client;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Arrange
+            _server = new TestServer(new WebHostBuilder()
+               .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [Test]
+        public async Task CheckIndexRoutine()
+        {
+            var response = await _client.GetAsync("/");
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Assert.IsTrue(response.IsSuccessStatusCode);
+        }
+
+        [Test]
+        public async Task CheckErrorPage()
+        {
+            var response = await _client.GetAsync("/PathToFileWhich/DoesntExit");
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
