8ca5fdfce687500c8f6d53095a6310b9659a98d3
PoloDev41 <paulalexandre.peyrat@gmail.com>
2019-11-13 19:30:23 +0100

    Start scheduler service

---
+++ WebCiv/WebCiv/Areas/ScheduleService/SchedulerHostedService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebCiv.Areas.ScheduleService
+{
+    /// <summary>
+    /// class to schedule any tasks
+    /// </summary>
+    public class SchedulerHostedService : IHostedService
+    {
+        /// <summary>
+        /// start service
+        /// </summary>
+        /// <param name="cancellationToken">cancellationToken</param>
+        /// <returns>completed task</returns>
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Stop the service
+        /// </summary>
+        /// <param name="cancellationToken">cancellation token</param>
+        /// <returns>completed task</returns>
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
